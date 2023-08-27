bd7a334cc5da3265b9b54e8d12ab7b752853d099
Julien <larroquejl@gmail.com>
2023-08-27 16:49:58 -0400

    added models, begin onetomany code

--- models/index.js
+++ models/index.js
@@ -5,8 +5,17 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
+
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'Cascade',
+
+});
 
 // Products belongToMany Tags (through ProductTag)
 
